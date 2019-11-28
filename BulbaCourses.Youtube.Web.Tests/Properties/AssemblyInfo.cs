a3a9e9d731cfd2692e4fce1859a6394dca77f125
Lyudmila Khomyakova <lv.khomyakova@gmail.com>
2019-11-28 21:00:37 +0300

    + BulbaCourses.Youtube.Web.Tests

---
+++ BulbaCourses.Youtube.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BulbaCourses.Youtube.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BulbaCourses.Youtube.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4428f775-3053-46f0-ae5f-b41fa69c0d6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BulbaCourses.Youtube.Web.Tests/VideoControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using Bogus;
+using BulbaCourses.Youtube.Web.Controllers;
+
+
+namespace BulbaCourses.Youtube.Web.Tests
+{
+    [TestFixture]
+    public class VideoControllerTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
