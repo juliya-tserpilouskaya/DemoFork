b005e862f85ec94e63e782932c48522e4ae0036a
Denis Makarchuk <hiedmant@gmail.com>
2019-11-29 22:47:58 +0300

    [*] Employee to Teacher

---
+++ BulbaCourses/BulbaCourses.TextMaterials_Presentations.Web/Models/StaffAndUsers/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BulbaCourses.TextMaterials_Presentations.Web.Models.Presentations;
+
+namespace BulbaCourses.TextMaterials_Presentations.Web.Models.StaffAndUsers
+{/// <summary>
+/// Employee info
+/// </summary>
+    public class Teacher
+    {
+        public IEnumerable<Presentation> ChangedPresentatons { get; set; }
+    }
+}
