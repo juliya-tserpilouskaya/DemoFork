bd202efcc388bace2567386062d3e084aba63dfe
Denis Makarchuk <hiedmant@gmail.com>
2019-11-29 23:03:43 +0300

    [+] Feedback class

---
+++ BulbaCourses/BulbaCourses.TextMaterials_Presentations.Web/Models/StaffAndUsers/Feedback.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulbaCourses.TextMaterials_Presentations.Web.Models.StaffAndUsers
+{
+    public class Feedback
+    {
+    }
+}
