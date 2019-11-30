5472d0d56fb97ab9e90c059a6d3385ecbdf89899
Fedor Saevich <f.saevich@gmail.com>
2019-11-30 21:37:58 +0300

    Add LearningCourseController actions

--- BulbaCourses.GlobalSearch.Web/Controllers/LearningCourseController.cs
+++ BulbaCourses.GlobalSearch.Web/Controllers/LearningCourseController.cs
@@ -11,6 +11,7 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
     [RoutePrefix("api/courses")]
     public class LearningCourseController : ApiController
     {
+        [HttpGet, Route("{id}")]
         public IHttpActionResult GetById(string id)
         {
             if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))//для ненужных _
@@ -27,5 +28,47 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
                 return InternalServerError(ex);
             }
         }
+        [HttpGet, Route("")]
+        public IHttpActionResult GetAll()
+        {
+            var result = LearningCourseStorage.GetAllCourses();
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
+        [HttpGet, Route("category/{domain}")]
+        public IHttpActionResult GetByCategory(string domain)
+        {
+            var result = LearningCourseStorage.GetByCategory(domain);
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
+        [HttpGet, Route("author/{id:int}")]
+        public IHttpActionResult GetByAuthor(int id)
+        {
+            var result = LearningCourseStorage.GetByAuthorId(id);
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
+        [HttpGet, Route("{id}/items")]
+        public IHttpActionResult GetItems(string id)
+        {
+            var result = LearningCourseStorage.GetLearningItemsByCourseId(id);
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
+        [HttpGet, Route("complexity/{level}")]
+        public IHttpActionResult GetByComplexity(string level)
+        {
+            var result = LearningCourseStorage.GetCourseByComplexity(level);
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
+        [HttpGet, Route("language/{lang}")]
+        public IHttpActionResult GetByLanguage(string lang)
+        {
+            var result = LearningCourseStorage.GetCourseByLanguage(lang);
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
     }
 }
