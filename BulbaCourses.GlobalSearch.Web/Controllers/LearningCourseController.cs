e3b85bcc09897610bd9918530f342070c52fa1e9
Fedor Saevich <f.saevich@gmail.com>
2019-11-30 20:19:59 +0300

    Add LearningCourseController

---
+++ BulbaCourses.GlobalSearch.Web/Controllers/LearningCourseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BulbaCourses.GlobalSearch.Web.Controllers
+{
+    public class LearningCourseController : ApiController
+    {
+
+    }
+}
