30b7bf7ffd5f4a8665ca68944490bbdcbce73b5b
Fedor Saevich <f.saevich@gmail.com>
2019-11-30 20:48:58 +0300

    Add LearningCourseController GetById action

--- BulbaCourses.GlobalSearch.Web/Controllers/LearningCourseController.cs
+++ BulbaCourses.GlobalSearch.Web/Controllers/LearningCourseController.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BulbaCourses.GlobalSearch.Web.Models;
 
 namespace BulbaCourses.GlobalSearch.Web.Controllers
 {
+    [RoutePrefix("api/courses")]
     public class LearningCourseController : ApiController
     {
-
+        public IHttpActionResult GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))//для ненужных _
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var result = LearningCourseStorage.GetById(id);
+                return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+            }
+            catch (InvalidOperationException ex) 
+            {
+                return InternalServerError(ex);
+            }
+        }
     }
 }
