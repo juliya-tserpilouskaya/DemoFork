d2ec179ac6761a5d7eb76575277652a948c5e95b
Fedor Saevich <f.saevich@gmail.com>
2019-12-01 18:22:53 +0300

    Document QueryStorage public methods

--- BulbaCourses.GlobalSearch.Web/Models/SearchQueryStorage.cs
+++ BulbaCourses.GlobalSearch.Web/Models/SearchQueryStorage.cs
@@ -29,30 +29,47 @@ namespace BulbaCourses.GlobalSearch.Web.Models
             }
         };
 
+        /// <summary>
+        /// Get all queries from the storage
+        /// </summary>
+        /// <returns>A collection of Search Queries</returns>
         public static IEnumerable<SearchQuery> GetAll()
         {
             return _queries.AsReadOnly();
         }
-
+        /// <summary>
+        /// Get all search queries from the storage
+        /// </summary>
+        /// <param name="id">Guid of query</param>
+        /// <returns>SearchQuery</returns>
         public static SearchQuery GetById(string id)
         {
             return _queries.SingleOrDefault(q => q.Id.Equals(id,
                 StringComparison.OrdinalIgnoreCase));
         }
-
+        /// <summary>
+        /// Add query to the storage
+        /// </summary>
+        /// <param name="query">Query to add</param>
+        /// <returns>Added query</returns>
         public static SearchQuery Add(SearchQuery query)
         {
             query.Id = Guid.NewGuid().ToString();
             _queries.Add(query);
             return query;
         }
-
+        /// <summary>
+        /// Remove storage from the storage by id
+        /// </summary>
+        /// <param name="id">Guid of query</param>
         public static void RemoveById(string id)
         {
             var query = _queries.SingleOrDefault(c => c.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
             _queries.Remove(query);
         }
-
+        /// <summary>
+        /// Remove all queries from storage
+        /// </summary>
         public static void RemoveAll()
         {
             _queries.Clear();
