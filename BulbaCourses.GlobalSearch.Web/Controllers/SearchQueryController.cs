54a135a6bce539b4bd9587f8162ac53006ff4acf
Fedor Saevich <f.saevich@gmail.com>
2019-12-01 13:32:09 +0300

    Add swagger description to Search Query services

--- BulbaCourses.GlobalSearch.Web/Controllers/SearchQueryController.cs
+++ BulbaCourses.GlobalSearch.Web/Controllers/SearchQueryController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BulbaCourses.GlobalSearch.Web.Models;
+using Swashbuckle.Swagger.Annotations;
 
 namespace BulbaCourses.GlobalSearch.Web.Controllers
 {
@@ -12,6 +13,8 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
     public class SearchQueryController : ApiController
     {
         [HttpGet, Route("")]
+        [SwaggerResponse(HttpStatusCode.NotFound, "There are no queries stored")]
+        [SwaggerResponse(HttpStatusCode.OK, "Queries are found", typeof(IEnumerable<SearchQuery>))]
         public IHttpActionResult GetAll()
         {
             var result = SearchQueryStorage.GetAll();
@@ -19,6 +22,10 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
         }
 
         [HttpGet, Route("{id}")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Ivalid query id")]
+        [SwaggerResponse(HttpStatusCode.NotFound, "The query doesn't exists")]
+        [SwaggerResponse(HttpStatusCode.OK, "The query is found", typeof(SearchQuery))]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, "Something went wrong")]
         public IHttpActionResult GetById(string id)
         {
             if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _))
@@ -36,7 +43,8 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
             }
         }
 
-        [HttpPost, Route("")] 
+        [HttpPost, Route("")]
+        [SwaggerResponse(HttpStatusCode.OK, "The query is added")]
         public IHttpActionResult Create([FromBody]SearchQuery query)
         {
             //validate here
@@ -44,6 +52,7 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
         }
 
         [HttpDelete, Route("")]
+        [SwaggerResponse(HttpStatusCode.OK, "The queries are removed")]
         public IHttpActionResult ClearAll()
         {
             SearchQueryStorage.RemoveAll();
@@ -51,6 +60,10 @@ namespace BulbaCourses.GlobalSearch.Web.Controllers
         }
 
         [HttpDelete, Route("{id}")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Ivalid query id")]
+        [SwaggerResponse(HttpStatusCode.NotFound, "The query doesn't exists")]
+        [SwaggerResponse(HttpStatusCode.OK, "The query is deleted")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, "Something went wrong")]
         public IHttpActionResult DeleteById(string id)
         {
             if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out var _) || SearchQueryStorage.GetById(id) == null)
