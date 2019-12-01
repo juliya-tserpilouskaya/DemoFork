1547af0dd8d7b3c77266468233298a092ab5be8f
Fedor Saevich <f.saevich@gmail.com>
2019-12-01 11:17:43 +0300

    Add SearchQueries classes

---
+++ BulbaCourses.GlobalSearch.Web/Models/SearchQueryStorage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulbaCourses.GlobalSearch.Web.Models
+{
+    public class SearchQueryStorage
+    {
+        public static List<SearchQuery> _queries = new List<SearchQuery>()
+        {
+            new SearchQuery
+            {
+                Id = Guid.NewGuid().ToString(),
+                Query = "basics c",
+                Date = DateTime.Now,
+            },
+            new SearchQuery
+            {
+                Id = Guid.NewGuid().ToString(),
+                Query = "c# advanced",
+                Date = DateTime.Now,
+            },
+            new SearchQuery
+            {
+                Id = Guid.NewGuid().ToString(),
+                Query = "php 9 podcast",
+                Date = DateTime.Now,
+            }
+        };
+
+        public static IEnumerable<SearchQuery> GetAll()
+        {
+            return _queries.AsReadOnly();
+        }
+
+        public static SearchQuery Add(SearchQuery query)
+        {
+            query.Id = Guid.NewGuid().ToString();
+            _queries.Add(query);
+            return query;
+        }
+
+        public static void RemoveById(string id)
+        {
+            var query = _queries.SingleOrDefault(c => c.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+            _queries.Remove(query);
+        }
+
+        public static void RemoveAll(string id)
+        {
+            _queries.Clear();
+        }
+
+    }
+}
