3261625e37090b3b2d58783dbfbbbb5cb6a86699
Andrei Valuika <andrei.valuika@gmail.com>
2019-12-02 11:45:00 +0000

    Video

---
+++ BulbaCourse.Video.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Video.Data.Models;
+
+namespace BulbaCourse.Video.Data.Interfaces
+{
+    public interface IUserRepository
+    {
+        User GetByLogin(string userName);
+        User GetUserById(string id);
+        IEnumerable<User> GetAll();
+        User Add(User user);
+        void Delete(User user);
+        void DeleteById(string userId);
+        ICollection<Course> GetUserCourse(string userId);
+        Role CheckRole(Role role);
+        bool AddRole(string newRole);
+        bool AddCourseToUser(string userId, string courseId);
+        bool DeleteCourseFromUser(string userId, string courseId);
+    }
+}
---
+++ BulbaCourse.Video.Infrastructure/Model/Course.cs
@@ -0,0 +1,26 @@
+﻿using BulbaCourse.Video.Infrastructure.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourse.Video.Infrastructure.Model
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string Name { get; set; }
+        public string CreatorId { get; set; }
+        public CourseLevel Level { get; set; } 
+        public double Raiting { get; set; }
+        public string Description { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public int Duration { get; set; }
+        public double Price { get; set; }
+
+        //public IList<VideoItem> Videos { get; set; }
+        //public ICollection<Comment> Comments { get; set; }
+        //public ICollection<Tag> Tags { get; set; }
+    }
+}
