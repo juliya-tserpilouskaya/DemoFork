263a7900c803c55ed44c85eb8cfbcdb63e038230
Fedor Saevich <f.saevich@gmail.com>
2019-12-03 22:26:20 +0300

    + Add ILearningCourse service methods

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/ILearningCourseService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/ILearningCourseService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BulbaCourses.GlobalSearch.Logic.Models;
 
 namespace BulbaCourses.GlobalSearch.Logic.InterfaceServices
 {
-    interface ILearningCourseService
+    public interface ILearningCourseService
     {
+        IEnumerable<LearningCourse> GetAllCourses();
+        LearningCourse GetById(string id);
+        IEnumerable<LearningCourse> GetByCategory(string category);
+        IEnumerable<LearningCourse> GetByAuthorId(int id);
+        IEnumerable<LearningCourseItem> GetLearningItemsByCourseId(string id);
+        IEnumerable<LearningCourse> GetCourseByComplexity(string complexity);
+        IEnumerable<LearningCourse> GetCourseByLanguage(string lang);
+        IEnumerable<LearningCourse> GetCourseByQuery(string query);
     }
 }
