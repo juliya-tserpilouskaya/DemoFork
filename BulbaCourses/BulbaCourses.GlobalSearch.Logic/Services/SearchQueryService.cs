e5fa0eb4988d25e21261f12d3dfebdf8e71a9cab
Fedor Saevich <f.saevich@gmail.com>
2019-12-03 22:44:33 +0300

    + Add SearchQueryService class methods

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/SearchQueryService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/SearchQueryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BulbaCourses.GlobalSearch.Logic.InterfaceServices;
+using BulbaCourses.GlobalSearch.Logic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace BulbaCourses.GlobalSearch.Logic.Services
 {
-    class SearchQueryService
+    public class SearchQueryService : ISearchQueryService
     {
+        public IEnumerable<SearchQuery> GetAll()
+        {
+            return SearchQueryStorage.GetAll();
+        }
+
+        public SearchQuery GetById(string id)
+        {
+            return SearchQueryStorage.GetById(id);
+        }
+
+        public SearchQuery Add(SearchQuery query)
+        {
+            return SearchQueryStorage.Add(query);
+        }
+
+        public void RemoveById(string id)
+        {
+            SearchQueryStorage.RemoveById(id);
+        }
+
+        public void RemoveAll()
+        {
+            SearchQueryStorage.RemoveAll();
+        }
     }
 }
