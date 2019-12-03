92b0148c36df7b2b90a4c20c64b0860547fa0710
Fedor Saevich <f.saevich@gmail.com>
2019-12-03 22:28:53 +0300

    + Add ISearchQuery service methods

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/ISearchQueryService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/ISearchQueryService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BulbaCourses.GlobalSearch.Logic.Models;
 
 namespace BulbaCourses.GlobalSearch.Logic.InterfaceServices
 {
-    interface ISearchQueryService
+    public interface ISearchQueryService
     {
+        IEnumerable<SearchQuery> GetAll();
+        SearchQuery GetById(string id);
+        SearchQuery Add(SearchQuery query);
+        void RemoveById(string id);
+        void RemoveAll();
     }
 }
