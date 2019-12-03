8fdf6a4ff463a43f14fd84f8bd835d9680b4b98d
Fedor Saevich <f.saevich@gmail.com>
2019-12-03 22:38:50 +0300

    + Add LearningCourseService class methods

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/LearningCourseService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/LearningCourseService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BulbaCourses.GlobalSearch.Logic.InterfaceServices;
+using BulbaCourses.GlobalSearch.Logic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace BulbaCourses.GlobalSearch.Logic.Services
 {
-    class LearningCourseService
+    public class LearningCourseService : ILearningCourseService
     {
+        public IEnumerable<LearningCourse> GetAllCourses()
+        {
+            return LearningCourseStorage.GetAllCourses();
+        }
+
+        public LearningCourse GetById(string id)
+        {
+            return LearningCourseStorage.GetById(id);
+        }
+
+        public IEnumerable<LearningCourse> GetByCategory(string category)
+        {
+            return LearningCourseStorage.GetByCategory(category);
+        }
+
+        public IEnumerable<LearningCourse> GetByAuthorId(int id)
+        {
+            return LearningCourseStorage.GetByAuthorId(id);
+        }
+
+        public IEnumerable<LearningCourseItem> GetLearningItemsByCourseId(string id)
+        {
+            return LearningCourseStorage.GetLearningItemsByCourseId(id);
+        }
+
+        public IEnumerable<LearningCourse> GetCourseByComplexity(string complexity)
+        {
+            return LearningCourseStorage.GetCourseByComplexity(complexity);
+        }
+
+        public IEnumerable<LearningCourse> GetCourseByLanguage(string lang)
+        {
+            return LearningCourseStorage.GetCourseByLanguage(lang);
+        }
+
+        public IEnumerable<LearningCourse> GetCourseByQuery(string query)
+        {
+            return LearningCourseStorage.GetCourseByQuery(query);
+        }
     }
 }
