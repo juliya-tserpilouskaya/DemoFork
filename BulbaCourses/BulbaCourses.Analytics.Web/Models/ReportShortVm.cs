a44d6404e82b0b5c6dfa7e117f657566c2468aae
Dmitriy <d.mace@live.com>
2019-12-05 22:17:02 +0300

    + add ViewModel

---
+++ BulbaCourses/BulbaCourses.Analytics.Web/Models/ReportShortVm.cs
@@ -0,0 +1,9 @@
+﻿namespace BulbaCourses.Analytics.Web.Models
+{
+    public class ReportShortVm
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ BulbaCourses/BulbaCourses.Analytics.Web/Models/ReportVm.cs
@@ -0,0 +1,11 @@
+﻿namespace BulbaCourses.Analytics.Web.Models
+{
+    public class ReportVm
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
