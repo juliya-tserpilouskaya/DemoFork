e0e7d76679bf49fba285ef7368c71e88e4f36bf9
Fedor Saevich <f.saevich@gmail.com>
2019-12-05 20:57:45 +0300

    + Add entities configurations

---
+++ BulbaCourses/BulbaCourses.GlobalSearch.Data/EntitiesConfiguration/CoursesConfiguration/PodcastCourseConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using BulbaCourses.GlobalSearch.Data.Models;
+using BulbaCourses.GlobalSearch.Data.Models.Courses;
+
+namespace BulbaCourses.GlobalSearch.Data.EntitiesConfiguration.CoursesConfiguration
+{
+    class PodcastCourseConfiguration : EntityTypeConfiguration<PodcastCourseDB>
+    {
+        public PodcastCourseConfiguration()
+        {
+                
+        }
+    }
+}
