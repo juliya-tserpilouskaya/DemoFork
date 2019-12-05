4e9a1e144d4cd42d19a41ad0625e635fea9aec5d
Fedor Saevich <f.saevich@gmail.com>
2019-12-05 21:25:51 +0300

    + Add dispose methods to service classes

--- BulbaCourses/BulbaCourses.GlobalSearch.Data/Services/ArticleCourseService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Data/Services/ArticleCourseService.cs
@@ -8,5 +8,42 @@ namespace BulbaCourses.GlobalSearch.Data.Services
 {
     class ArticleCourseService
     {
+        private GlobalSearchContext _context = new GlobalSearchContext();
+
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+
+
+        //public BookDb GetById(string id)
+        //{
+        //    var course = _context.Books.SingleOrDefault(b => b.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+        //    return course;
+        //}
+
+        ~ArticleCourseService()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool flag)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _context.Dispose();
+            _isDisposed = true;
+
+            if (flag)
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
     }
 }
--- BulbaCourses/BulbaCourses.GlobalSearch.Data/Services/ExcerciseCourseService.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Data/Services/ExcerciseCourseService.cs
@@ -8,5 +8,34 @@ namespace BulbaCourses.GlobalSearch.Data.Services
 {
     class ExcerciseCourseService
     {
+        private GlobalSearchContext _context = new GlobalSearchContext();
+
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        ~ExcerciseCourseService()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool flag)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _context.Dispose();
+            _isDisposed = true;
+
+            if (flag)
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
     }
 }
