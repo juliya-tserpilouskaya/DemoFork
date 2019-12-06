6feff5c7e7464f3b27ad1ffff840e425cb797d11
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2019-12-06 12:57:24 +0300

    * Models files replased from Web to Logic

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/Models/AnonymousUser.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/Models/AnonymousUser.cs
@@ -1,10 +1,9 @@
-﻿using BulbaCourses.GlobalSearch.Logic.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace BulbaCourses.GlobalSearch.Web.Models
+namespace BulbaCourses.GlobalSearch.Logic.Models
 {
     public class AnonymousUser
     {
