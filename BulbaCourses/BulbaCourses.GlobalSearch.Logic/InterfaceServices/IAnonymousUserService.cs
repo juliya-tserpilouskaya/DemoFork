2177e44411e42dd9dfac27c9b2184ddb973acf6f
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2019-12-06 13:21:41 +0300

    + AnonymousUser Service implemented

---
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/IAnonymousUserService.cs
@@ -0,0 +1,18 @@
+﻿using BulbaCourses.GlobalSearch.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.GlobalSearch.Logic.InterfaceServices
+{
+    interface IAnonymousUserService
+    {
+        IEnumerable<AnonymousUser> GetAll();
+        AnonymousUser GetById(string id);
+        AnonymousUser Add(AnonymousUser anonymousUser);
+        void RemoveById(string id);
+        void RemoveAll();
+    }
+}
