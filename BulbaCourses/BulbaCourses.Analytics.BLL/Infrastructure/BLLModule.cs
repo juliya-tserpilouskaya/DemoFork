6645625de49b0a8544194fd7c61eb71809b7eb78
Dmitriy <d.mace@live.com>
2019-12-09 00:30:22 +0300

    + changed validation

--- BulbaCourses/BulbaCourses.Analytics.BLL/Infrastructure/BLLModule.cs
+++ BulbaCourses/BulbaCourses.Analytics.BLL/Infrastructure/BLLModule.cs
@@ -1,13 +1,5 @@
-﻿using BulbaCourses.Analytics.BLL.DTO;
+﻿using BulbaCourses.Analytics.BLL.Interface;
 using BulbaCourses.Analytics.BLL.Services;
-using BulbaCourses.Analytics.DAL.Interfaces;
-using BulbaCourses.Analytics.DAL.Models;
-using BulbaCourses.Analytics.DAL.Repositories;
-using BulbaCourses.Analytics.DAL.Storage;
-using BulbaCourses.Analytics.Infrastructure.BLL.Models;
-using BulbaCourses.Analytics.Infrastructure.BLL.Services;
-using BulbaCourses.Analytics.Infrastructure.DAL;
-using BulbaCourses.Analytics.Infrastructure.DAL.Models;
 using Ninject.Modules;
 
 namespace BulbaCourses.Analytics.BLL.Infrastructure
@@ -15,22 +7,9 @@ namespace BulbaCourses.Analytics.BLL.Infrastructure
     public class BLLModule : NinjectModule
     {
         public override void Load()
-        {
-            // Bind BLL
-            Bind<IReportDto>().To<ReportDto>();
+        {            
+            Bind<IValidation>().To<Validation>().InSingletonScope();
             Bind<IReportService>().To<ReportService>();
-
-            // Bind DAL
-            Bind<IReportDb>().To<ReportDb>();
-            Bind<IDashboardDb>().To<DashboardDb>();
-
-            Bind<IReportStorage>().To<ReportStorage>();
-            Bind<IReportRepository>().To<ReportRepository>();
-
-            Bind<IDashboardStorage>().To<DashboardStorage>();
-            Bind<IDashboardRepository>().To<DashboardRepository>();
-
-            Bind<IDataBase>().To<DataBase>();
         }
     }
 }
--- BulbaCourses/BulbaCourses.Analytics.BLL/Infrastructure/Validation.cs
+++ BulbaCourses/BulbaCourses.Analytics.BLL/Infrastructure/Validation.cs
@@ -1,35 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using BulbaCourses.Analytics.BLL.Interface;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BulbaCourses.Analytics.BLL.Infrastructure
 {
-    public static class Validation
+    public class Validation : IValidation
     {
-        public static Dictionary<string, string> Error { get; private set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Error { get; private set; } = new Dictionary<string, string>();
 
-        public static bool IsErrors => Error.Any();
+        public bool IsErrors => Error.Any();
 
-        public static void Free()
+        public void Init()
         {
             Error.Clear();
         }
 
-        public static bool IsNull(string Id, string param, string message)
+        public bool IsNull(string id, string param, string message)
         {
-            return string.IsNullOrWhiteSpace(Id) ? AddError(param, message) : false;
+            return string.IsNullOrWhiteSpace(id) ? AddError(param, message) : false;
         }
 
-        public static bool IsNull(object Obj, string param, string message)
+        public bool IsNull(object obj, string param, string message)
         {
-            return (Obj == null) ? AddError(param, message) : false;
+            return (obj == null) ? AddError(param, message) : false;
         }
 
-        public static bool IsEmty(bool Value, string param, string message)
+        public bool IsEmty(bool value, string param, string message)
         {
-            return (Value) ? AddError(param, message) : false;
+            return (value) ? AddError(param, message) : false;
         }
 
-        public static bool AddError(string param, string message)
+        public bool IsZero(int count, string param, string message)
+        {
+            return (count == 0) ? AddError(param, message) : false;
+        }
+
+        public bool AddError(string param, string message)
         {
             Error.Add(param, message);
             return true;
