149d742a8f10066464ed6334ff95f3e0a6f13ece
Dmitriy <d.mace@live.com>
2019-12-09 00:35:33 +0300

    + added resource

--- BulbaCourses/BulbaCourses.Analytics.BLL/Resources/Resource.Designer.cs
+++ BulbaCourses/BulbaCourses.Analytics.BLL/Resources/Resource.Designer.cs
@@ -22,7 +22,7 @@ namespace BulbaCourses.Analytics.BLL.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resource {
+    public class Resource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace BulbaCourses.Analytics.BLL.Resources {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BulbaCourses.Analytics.BLL.Resources.Resource", typeof(Resource).Assembly);
@@ -51,7 +51,7 @@ namespace BulbaCourses.Analytics.BLL.Resources {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,20 +61,29 @@ namespace BulbaCourses.Analytics.BLL.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Not Found Report.
+        ///   Looks up a localized string similar to Report doesn&apos;t exists.
         /// </summary>
-        internal static string NotFoundReport {
+        public static string NotFoundReport {
             get {
                 return ResourceManager.GetString("NotFoundReport", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Not Found Report (by Id).
+        ///   Looks up a localized string similar to Report doesn&apos;t exists (by Id).
         /// </summary>
-        internal static string NotFoundReportById {
+        public static string NotFoundReportById {
             get {
                 return ResourceManager.GetString("NotFoundReportById", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reports doesn&apos;t exist.
+        /// </summary>
+        public static string NotFoundReports {
+            get {
+                return ResourceManager.GetString("NotFoundReports", resourceCulture);
             }
         }
     }
