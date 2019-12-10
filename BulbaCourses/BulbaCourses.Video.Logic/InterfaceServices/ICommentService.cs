82446dd553fecdfce3163d49b5e3a639c83042d1
KozyrickiSergey <skozyricki@gmail.com>
2019-12-10 23:58:38 +0300

    * change logic folder position

---
+++ BulbaCourses/BulbaCourses.Video.Logic/InterfaceServices/ICommentService.cs
@@ -0,0 +1,19 @@
+﻿using BulbaCourses.Video.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.Video.Logic.InterfaceServices
+{
+    public interface ICommentService
+    {
+        CommentInfo GetById(string commentId);
+        IEnumerable<CommentInfo> GetAll();
+        void Add(CommentInfo comment);
+        CommentInfo UpdateCommentText(string commentId, string newText);
+        void RemoveById(string commentId);
+
+    }
+}
