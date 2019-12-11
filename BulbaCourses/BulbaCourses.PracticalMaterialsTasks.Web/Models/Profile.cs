62055d2544343f0cf06dda12a59e20902c042a94
Roman Morozov <rmarozau@inbox.ru>
2019-12-11 14:57:31 +0300

    + add Models(User,profile,settings,Task)

---
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/Models/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulbaCourses.PracticalMaterialsTasks.Web.Models
+{
+    [Table("Profile")]
+    public class Profile
+    {
+        [Key]
+        [Required]
+        public string ProfileId { get; set; } = Guid.NewGuid().ToString();
+
+        public string Progress { get; set; }
+
+        public string Rank { get; set; }
+
+        public int Complited { get; set; }
+
+        public int UserId { get; set; }
+
+    }
+}
---
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/Models/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulbaCourses.PracticalMaterialsTasks.Web.Models
+{
+    [Table("Settings")]
+    public class Settings
+    {
+  
+       public string SettingsId { get; set; } = Guid.NewGuid().ToString();
+
+       public int UserLvlChange { get; set; }
+
+       public int UserId { get; set; } 
+
+
+
+
+    }
+}
