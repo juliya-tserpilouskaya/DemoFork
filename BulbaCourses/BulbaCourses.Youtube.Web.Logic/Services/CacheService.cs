ca5adb4e63cad169e5c029c3b61ac616f8fe9f50
e.patysh <e.patysh@mail.com>
2019-12-11 13:38:09 +0300

    [+] add CacheService

---
+++ BulbaCourses/BulbaCourses.Youtube.Web.Logic/Services/CacheService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using BulbaCourses.Youtube.Web.Logic.Models;
+
+namespace BulbaCourses.Youtube.Web.Logic.Services
+{
+    public class CacheService
+    {
+        public SearchRequest GetValue(int? id)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Get(id.ToString()) as SearchRequest;
+        }
+
+        public bool Add(SearchRequest value)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            return memoryCache.Add(value.Id.ToString(), value, DateTime.Now.AddMinutes(10));
+        }
+
+        public void Update(SearchRequest value)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            memoryCache.Set(value.Id.ToString(), value, DateTime.Now.AddMinutes(10));
+        }
+
+        public void Delete(int id)
+        {
+            MemoryCache memoryCache = MemoryCache.Default;
+            if (memoryCache.Contains(id.ToString()))
+            {
+                memoryCache.Remove(id.ToString());
+            }
+        }
+    }
+}
--- BulbaCourses/BulbaCourses.Youtube.Web.Logic/Services/LogicService.cs
+++ BulbaCourses/BulbaCourses.Youtube.Web.Logic/Services/LogicService.cs
@@ -7,6 +7,7 @@ using BulbaCourses.Youtube.Web.DataAccess.Models;
 using BulbaCourses.Youtube.Web.Logic.Models;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
+using System.Runtime.Caching;
 
 namespace BulbaCourses.Youtube.Web.Logic.Services
 {
@@ -15,6 +16,9 @@ namespace BulbaCourses.Youtube.Web.Logic.Services
         IStoryService _storyService;
         IVideoService _videoService;
         ISearchRequestService _requestService;
+
+        private CacheService _cache = new CacheService();
+
         public LogicService(IStoryService storyService, 
             IVideoService videoService, ISearchRequestService requestService)
         {
@@ -25,51 +29,64 @@ namespace BulbaCourses.Youtube.Web.Logic.Services
         public IEnumerable<ResultVideoDb> SearchRun(SearchRequest searchRequest)
         {
             //convert searchRequest to searchRequestDb
-            SearchRequestDb searchRequestDb = new SearchRequestDb();
-            searchRequestDb.Title = searchRequest.Title;
-
-            
-            if(!_requestService.Exists(searchRequestDb))
-                searchRequestDb = _requestService.Save(searchRequestDb);
-
-            // Create the service.
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            SearchRequestDb searchRequestDb = new SearchRequestDb()
             {
-                ApiKey = "AIzaSyAZQwG0x87WljOsVjJGfL1fIC30EWf42pg",
-                ApplicationName = "BulbaCourses"
-            });
-
-            // Run the request.
-            var searchListRequest = youtubeService.Search.List("snippet");
-            searchListRequest.Q = searchRequest.Title;
-            searchListRequest.MaxResults = 10;
-
-            // Call the search.list method to retrieve results matching the specified searchRequest
-            var searchListResponse = searchListRequest.Execute();
-
+                Title = searchRequest.Title,
+                VideoId = searchRequest.VideoId,
+                Author = searchRequest.Author
+            };
 
             List<ResultVideoDb> resultVideos = new List<ResultVideoDb>();
-
-            foreach (var searchResult in searchListResponse.Items)
+            
+            
+            //save request if dont exists in the db and get query to youtube
+            if (!_requestService.Exists(searchRequestDb))
             {
-                ResultVideoDb resultVideo = new ResultVideoDb();
-                resultVideo.Id = searchResult.Id.VideoId;
-                resultVideo.Etag = searchResult.ETag;
-                resultVideo.Title = searchResult.Snippet.Title;
-                resultVideo.Channel = new ChannelDb()
-                {
-                    Id = searchResult.Snippet.ChannelId,
-                    Name = searchResult.Snippet.ChannelTitle,
-                    Mentor = new MentorDb(),
-                    Videos = new List<ResultVideoDb>()
-                };
-                resultVideo.PublishedAt = searchResult.Snippet.PublishedAt;
-                resultVideo.Description = searchResult.Snippet.Description;
-                resultVideo.SearchRequests = new List<SearchRequestDb>();
-                // resultVideo.SearchRequests.Add(searchRequestDb.Id);       //write searchRequestId... !!!
+                _requestService.Save(searchRequestDb);
 
-                resultVideos.Add(resultVideo);
+                // Create the service.
+                var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyAZQwG0x87WljOsVjJGfL1fIC30EWf42pg",
+                    ApplicationName = "BulbaCourses"
+                });
+
+                // Run the request.
+                var searchListRequest = youtubeService.Search.List("snippet");
+                searchListRequest.Q = searchRequest.Title;
+                searchListRequest.MaxResults = 10;
+
+                // Call the search.list method to retrieve results matching the specified searchRequest
+                var searchListResponse = searchListRequest.Execute();
+
+                foreach (var searchResult in searchListResponse.Items)
+                {
+                    ResultVideoDb resultVideo = new ResultVideoDb();
+                    resultVideo.Id = searchResult.Id.VideoId;
+                    resultVideo.Etag = searchResult.ETag;
+                    resultVideo.Title = searchResult.Snippet.Title;
+                    resultVideo.Channel = new ChannelDb()
+                    {
+                        Id = searchResult.Snippet.ChannelId,
+                        Name = searchResult.Snippet.ChannelTitle,
+                        Mentor = new MentorDb(),
+                        Videos = new List<ResultVideoDb>()
+                    };
+                    resultVideo.PublishedAt = searchResult.Snippet.PublishedAt;
+                    resultVideo.Description = searchResult.Snippet.Description;
+                    resultVideo.SearchRequests = new List<SearchRequestDb>();
+                    // resultVideo.SearchRequests.Add(searchRequestDb.Id);       //write searchRequestId... !!!
+
+                    resultVideos.Add(resultVideo);
+                }
+            }                
+            else if (_cache.GetValue(searchRequest.Id) != null)
+            {
+                
+
             }
+
+            
 
 
             return resultVideos.AsReadOnly();
