3bb804e4db935af559e70e5a387932ab5bb20267
Dmitriy <d.mace@live.com>
2019-12-21 23:18:32 +0300

    +added Infrastructure project

---
+++ BulbaCourses/BulbaCourses.Analytics.Infrastructure/Models/DashboardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BulbaCourses.Analytics.Infrastructure.Models
+{
+    public class DashboardDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
