e62a088e626533fd25e24c8b921fc26086932202
Fedor Saevich <f.saevich@gmail.com>
2019-12-21 12:19:26 +0300

    + Add search controller and service

---
+++ BulbaCourses.GlobalSearch.Web/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BulbaCourses.GlobalSearch.Web.Controllers
+{
+    [RoutePrefix("api/search")]
+    public class SearchController : ApiController
+    {
+
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+    }
+}
---
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/SearchService.cs
@@ -0,0 +1,23 @@
+﻿using BulbaCourses.GlobalSearch.Logic.DTO;
+using BulbaCourses.GlobalSearch.Logic.InterfaceServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.GlobalSearch.Logic.Services
+{
+    class SearchService : ISearchService
+    {
+        public IEnumerable<LearningCourseDTO> Search()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LearningCourseDTO>> SearchAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
