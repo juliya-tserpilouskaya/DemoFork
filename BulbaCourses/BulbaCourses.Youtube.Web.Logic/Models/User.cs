67b5744eb4016041b6a99b7efea6c81a7f6077ad
Lyudmila Khomyakova <lv.khomyakova@gmail.com>
2019-12-27 15:15:43 +0300

    + ResultVideoValidator.cs, UserValidator.cs

--- BulbaCourses/BulbaCourses.Youtube.Web.Logic/Models/User.cs
+++ BulbaCourses/BulbaCourses.Youtube.Web.Logic/Models/User.cs
@@ -8,11 +8,11 @@ namespace BulbaCourses.Youtube.Web.Logic.Models
     {
         public int? Id { get; set; }
         public string Login { get; set; }
-        public string Password { get; set; }  //скрыть надо бы..
+        public string Password { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName { get; set; }
-        public string NumberPhone { get; set; } //возможно надо для авторизации, или восстановления пароля
+        public string NumberPhone { get; set; } 
         public string Email { get; set; }
         public string ReserveEmail { get; set; }
     }
