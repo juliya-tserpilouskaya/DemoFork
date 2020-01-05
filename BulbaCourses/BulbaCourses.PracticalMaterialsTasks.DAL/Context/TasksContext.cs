ae01ae6bda2335bb08a34720283a96cea9280738
Morozov R.A <rmarozau@inbox.ru>
2020-01-05 19:52:19 +0300

    + Update OnModelCreating

--- BulbaCourses/BulbaCourses.PracticalMaterialsTasks.DAL/Context/TasksContext.cs
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.DAL/Context/TasksContext.cs
@@ -22,6 +22,21 @@ namespace BulbaCourses.PracticalMaterialsTasks.DAL.Context
         {
             connectionString = _connectionString;
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskDb>().ToTable("Tasks").HasKey(t => t.Id);
+            modelBuilder.Entity<TaskDb>().Property(t => t.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<TaskDb>().Property(t => t.TaskLevel).IsRequired();
+            modelBuilder.Entity<TaskDb>().Property(t => t.Text).IsRequired();
+
+            modelBuilder.Entity<UserDb>().ToTable("Users").HasKey(t => t.Id);
+            modelBuilder.Entity<UserDb>().Property(t => t.LastName).IsRequired().HasMaxLength(30);
+            modelBuilder.Entity<UserDb>().Property(t => t.FirstName).IsRequired().HasMaxLength(30);
+            modelBuilder.Entity<UserDb>().Property(t => t.NickName).IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<UserDb>().Property(t => t.Password).IsRequired().HasMaxLength(20);
+            
+        }
     }
     public class DbInitializer : DropCreateDatabaseIfModelChanges<TasksContext>
     {
