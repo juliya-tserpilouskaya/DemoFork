c9a666227e01a7d62e65266ce26afc6417657454
Roman Morozov <rmarozau@inbox.ru>
2020-01-09 15:54:20 +0300

    + add TaskController

--- BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/Controllers/TaskController.cs
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/Controllers/TaskController.cs
@@ -5,15 +5,33 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Filters;
+using System.Web;
 using EasyNetQ;
 using FluentValidation;
 using FluentValidation.WebApi;
+using BulbaCourses.PracticalMaterialsTasks.BLL.Interfaces;
 
 
 
 namespace BulbaCourses.PracticalMaterialsTasks.WEB.Controllers
 {
-    public class TaskController
+    [RoutePrefix("api/Tasks")]
+   // [Authorize]
+    public class TaskController: ApiController
     {
+        private readonly ITaskService _taskservice;
+        
+        public TaskController(ITaskService taskService)
+        {
+            _taskservice = taskService;
+        }
+
+        [HttpGet,Route("")]
+        public IHttpActionResult GetAll()
+        {
+            var result = _taskservice.GetTasks();
+            return result == null ? NotFound() : (IHttpActionResult)Ok(result);
+        }
+
     }
 }
