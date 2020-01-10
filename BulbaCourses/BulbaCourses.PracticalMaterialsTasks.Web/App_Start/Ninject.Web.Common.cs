dbc15a23e1cd24593bdf9fa477c8b255e6a1a4d3
Roman Morozov <rmarozau@inbox.ru>
2020-01-10 12:11:28 +0300

    + Udp web(swagger)

--- BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/App_Start/Ninject.Web.Common.cs
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.Web/App_Start/Ninject.Web.Common.cs
@@ -5,12 +5,13 @@ namespace BulbaCourses.PracticalMaterialsTasks.WEB.App_Start
 {
     using System;
     using System.Web;
-
+    using System.Web.Http;
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
     using Ninject.Web.Common.WebHost;
+    using Ninject.Web.WebApi;
 
     public static class NinjectWebCommon 
     {
@@ -45,6 +46,7 @@ namespace BulbaCourses.PracticalMaterialsTasks.WEB.App_Start
             {
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
                 RegisterServices(kernel);
                 return kernel;
             }
