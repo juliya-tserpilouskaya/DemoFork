1791c732f9dc4b2b017e314655830ca16fe0f078
Roman Morozov <rmarozau@inbox.ru>
2020-01-10 15:16:39 +0300

    + Update web layer

---
+++ BulbaCourses/BulbaCourses.PracticalMaterialsTasks.DAL/DataModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.PracticalMaterialsTasks.DAL
+{
+    public  class DataModule:
+    {
+        public override void Load()
+        {
+            Bind<BookContext>().ToSelf();
+        }
+    }
+}
