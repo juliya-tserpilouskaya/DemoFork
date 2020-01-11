d9fa5d0522d94fe62b075a8bd575536f66e0d21e
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2020-01-11 22:00:40 +0300

    + DTO files for Bookmark and user added * DTO file for SearchQuery changed (userId added)

--- BulbaCourses/BulbaCourses.GlobalSearch.Logic/DTO/BookmarkDTO.cs
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/DTO/BookmarkDTO.cs
@@ -1,6 +1,4 @@
-﻿using BulbaCourses.GlobalSearch.Logic.Validators;
-using FluentValidation.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace BulbaCourses.GlobalSearch.Logic.DTO
 {
-    public class SearchQueryDTO
+    public class BookmarkDTO
     {
         public string Id { get; set; }
-        public string Query { get; set; }
-        public DateTime? Date { get; set; }
         public string UserId { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; }
     }
 }
