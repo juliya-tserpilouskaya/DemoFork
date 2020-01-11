a28399edbd899db6ab35680d12e1d7c3290f3c56
Juliya Terpilovskaya <juliya.tserpilouskaya@gmail.com>
2020-01-11 23:36:09 +0300

    + add User services

---
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/InterfaceServices/IUserService.cs
@@ -0,0 +1,84 @@
+﻿using BulbaCourses.GlobalSearch.Logic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.GlobalSearch.Logic.InterfaceServices
+{
+    interface IUserService
+    {
+        /// <summary>
+        /// Returns all users
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<UserDTO> GetAll();
+
+        /// <summary>
+        /// Asynchronously returns all users
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<UserDTO>> GetAllAsync();
+
+        /// <summary>
+        /// Returns user by id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        UserDTO GetById(string id);
+
+        /// <summary>
+        /// Asynchronously returns user by id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        Task<UserDTO> GetByIdAsync(string id);
+
+        /// <summary>
+        /// Creates new user
+        /// </summary>
+        /// <param name="user">New user</param>
+        /// <returns></returns>
+        UserDTO Add(UserDTO user);
+
+        /// <summary>
+        /// Returns bookmarks by user id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        IEnumerable<BookmarkDTO> GetBookmarksByUserId(string id);
+
+        /// <summary>
+        /// Asynchronously returns bookmarks by user id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        Task<IEnumerable<BookmarkDTO>> GetBookmarksByUserIdAsync(string id);
+
+        /// <summary>
+        /// Returns search queries by user id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        IEnumerable<SearchQueryDTO> GetSearchQueriesByUserId(string id);
+
+        /// <summary>
+        /// Asynchronously returns search queries by user id
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns></returns>
+        Task<IEnumerable<SearchQueryDTO>> GetSearchQueriesByUserIdAsync(string id);
+
+        /// <summary>
+        /// Removes user by id
+        /// </summary>
+        /// <param name="id">User id</param>
+        void RemoveById(string id);
+
+        /// <summary>
+        /// Removes all users from database
+        /// </summary>
+        void RemoveAll();
+    }
+}
---
+++ BulbaCourses/BulbaCourses.GlobalSearch.Logic/Services/UserService.cs
@@ -0,0 +1,68 @@
+﻿using BulbaCourses.GlobalSearch.Logic.DTO;
+using BulbaCourses.GlobalSearch.Logic.InterfaceServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbaCourses.GlobalSearch.Logic.Services
+{
+    class UserService : IUserService
+    {
+        public IEnumerable<UserDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookmarkDTO> GetBookmarksByUserId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BookmarkDTO>> GetBookmarksByUserIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDTO GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SearchQueryDTO> GetSearchQueriesByUserId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SearchQueryDTO>> GetSearchQueriesByUserIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDTO Add(UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
