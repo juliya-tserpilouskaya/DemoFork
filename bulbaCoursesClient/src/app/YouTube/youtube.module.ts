5390366a75929de4138b099e6204dcb25e20da1b
e.patysh <e.patysh@mail.com>
2020-01-14 15:43:48 +0300

    [*] Merged with develop

--- bulbaCoursesClient/src/app/YouTube/youtube.module.ts
+++ bulbaCoursesClient/src/app/YouTube/youtube.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsComponent } from './components/components.component';
-import { YoutubeComponent } from './youtube.component';
-import { MenuComponent } from './components/menu/menu/menu.component';
 import { SearchRequestComponent } from './components/search-request/search-request.component';
 import { SearchStoryComponent } from './components/search-story/search-story.component';
 import { SearchResultComponent } from './components/search-result/search-result.component';
@@ -11,7 +8,7 @@ import { VideoComponent } from './components/video/video.component';
 
 
 @NgModule({
-  declarations: [ComponentsComponent, YoutubeComponent, MenuComponent, SearchRequestComponent, SearchStoryComponent, SearchResultComponent, VideoComponent],
+  declarations: [SearchRequestComponent, SearchStoryComponent, SearchResultComponent, VideoComponent],
   imports: [
     CommonModule
   ]
--- bulbaCoursesClient/src/app/app.module.ts
+++ bulbaCoursesClient/src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SampleModule } from './sample/sample.module';
-import { YoutubeComponent } from './YouTube/conponents/youtube/youtube.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    YoutubeComponent
   ],
   imports: [
     BrowserModule,
