cfc2586a32951032a1ca66107e01fd823045c614
Denis Makarchuk <hiedmant@gmail.com>
2020-01-20 16:15:36 +0000

    Presentations loading

---
+++ BulbaCourses/BulbaCourses.TextMaterials_Presentations.Logic/Interfaces/IFeedbacksService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Presentations.Logic.Repositories;
+using System.Threading.Tasks;
+using Presentations.Logic.Models;
+
+namespace Presentations.Logic.Interfaces
+{
+    public interface IFeedbacksService : IDisposable
+    {
+        Task<Result<Feedback>> AddFeedbackAsync(FeedbackAdd_DTO model);
+
+        Task<Feedback> GetFeedbackByIdAsync(string id);
+
+        Task<IEnumerable<Feedback>> GetAllFeedbacksAsync();
+
+        Task<Result<Feedback>> UpdateFeedbackAsync(Feedback model);
+
+        Task<Result> DeleteFeedbackByIdAsync(string id);
+    }
+}
