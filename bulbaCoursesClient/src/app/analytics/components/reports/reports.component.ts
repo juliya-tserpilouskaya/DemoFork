e449297e58c6e6cc739aa9b3353c80b391a2dbe6
Dmitriy <d.mace@live.com>
2020-01-25 23:37:25 +0300

    + added confirmation dialog, updated operation update, delete, added unsubscribe (front)

--- bulbaCoursesClient/src/app/analytics/components/reports/reports.component.ts
+++ bulbaCoursesClient/src/app/analytics/components/reports/reports.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CustomUser } from 'src/app/auth/models/user';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { ReportsService } from '../../services/reports.service';
-import { ReportShort } from '../models/reportShort';
 import { Dropdown } from 'primeng/dropdown/public_api';
 import { Button } from 'primeng/button/button';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
+import { ReportsService } from '../../services/reports.service';
+import { ReportShort } from '../models/reportShort';
+import { ConfirmationDialogService } from '../../ensure/dialog/confirmdialog/confirmdialog.service';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: [ './reports.component.scss' ]
 })
-export class ReportsComponent implements OnInit {
+export class ReportsComponent implements OnInit, OnDestroy {
+
+  private subscription$: Subscription = new Subscription();
 
   isAuthenticated: boolean;
   user: CustomUser;
@@ -24,14 +29,15 @@ export class ReportsComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private reportsService: ReportsService,
-    private loader: NgxUiLoaderService
+    private loader: NgxUiLoaderService,
+    private confirmationDialogService: ConfirmationDialogService,
+    private messageService: MessageService
     ) {
-
   }
 
   ngOnInit() {
-    this.authService.isAuthenticated$.subscribe((flag) => this.isAuthenticated = flag);
-    this.authService.user$.subscribe((user) => this.user = user as CustomUser);
+    this.subscription$.add( this.authService.isAuthenticated$.subscribe((flag) => this.isAuthenticated = flag) );
+    this.subscription$.add( this.authService.user$.subscribe((user) => this.user = user as CustomUser) );
 
     this.getReports();
 
@@ -42,29 +48,45 @@ export class ReportsComponent implements OnInit {
       {separator: true},
       {label: 'Edit', icon: 'pi pi-cog', command: () => {
         this.editReport();
-    }}
-  ];
+     }}
+    ];
   }
 
-  getReports() {
-      console.log('getReports');
-      this.loader.start();
-      const sub = this.reportsService.getReports().subscribe(
-      data => {this.reportShorts = data; console.log('data loaded'); this.loader.stop(); },
-      () => console.log('Error getReports'),
-      () => sub.unsubscribe);
+  getReports( complited: CallableFunction = null) {
+      console.log('Get Reports');
+      if (complited == null) { this.loader.start(); }
+      this.reportsService.getReports().pipe(first()).subscribe(
+        data => {
+          this.reportShorts = data;
+          console.log('Data loaded');
+          if (complited == null) { this.loader.stop(); }
+        },
+        () => console.log('Error getReports'),
+        () => complited == null ? null : complited()
+      );
   }
 
   deleteReport() {
-    console.log(this.selectedReport.Id.toString());
-    return;
-    const sub = this.reportsService.deleteReport(this.selectedReport.Id.toString()).subscribe(
-      () => null,
-      () => console.log('Error deleteReport'),
+    const reportName = this.selectedReport.Name.toString();
+    const reportId = this.selectedReport.Id.toString();
+
+    this.confirmationDialogService.confirm(
+      'Delete Report',
+      reportName + '<p>Are you sure you want to delete this report?</p>',
       () => {
-        this.getReports();
-      }
-    );
+        console.log('Delete');
+        const msg = {severity: 'success', summary: 'Deleted', detail: reportName};
+        this.reportsService.deleteReport(reportId).pipe(first()).subscribe(
+          () => null,
+          () => console.log('Error delete Report'),
+          () => {
+            this.getReports(
+                () => this.messageService.add(msg)
+              );
+          }
+        );
+      },
+      () => console.log('Cancel delete.'));
   }
 
   editReport() {
@@ -74,11 +96,15 @@ export class ReportsComponent implements OnInit {
 
   changeReport() {
     if (this.selectedReport == null) { return; }
-    console.log(this.selectedReport);
+    console.log('Select Report.');
   }
 
   clearFilter(dropdown: Dropdown, button: Button) {
     dropdown.resetFilter();
     button.disabled = true;
-}
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
+  }
 }
