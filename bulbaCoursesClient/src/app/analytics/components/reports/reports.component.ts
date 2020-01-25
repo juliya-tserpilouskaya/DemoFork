a4d99fae26845a937867ced59e446090d92ea352
Dmitriy <d.mace@live.com>
2020-01-25 18:47:32 +0300

    * changed report component (front)

--- bulbaCoursesClient/src/app/analytics/components/reports/reports.component.ts
+++ bulbaCoursesClient/src/app/analytics/components/reports/reports.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomUser } from 'src/app/auth/models/user';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ReportsService } from '../../services/reports.service';
 import { ReportShort } from '../models/reportShort';
+import { Dropdown } from 'primeng/dropdown/public_api';
+import { Button } from 'primeng/button/button';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-reports',
@@ -13,9 +17,15 @@ export class ReportsComponent implements OnInit {
 
   isAuthenticated: boolean;
   user: CustomUser;
+  selectedReport: ReportShort;
   reportShorts: ReportShort[] = [];
+  items: MenuItem[];
 
-  constructor(private authService: AuthService, private reportsService: ReportsService) {
+  constructor(
+    private authService: AuthService,
+    private reportsService: ReportsService,
+    private loader: NgxUiLoaderService
+    ) {
 
   }
 
@@ -24,17 +34,31 @@ export class ReportsComponent implements OnInit {
     this.authService.user$.subscribe((user) => this.user = user as CustomUser);
 
     this.getReports();
+
+    this.items = [
+      {label: 'Delete', icon: 'pi pi-times', command: () => {
+          this.deleteReport();
+      }},
+      {separator: true},
+      {label: 'Edit', icon: 'pi pi-cog', command: () => {
+        this.editReport();
+    }}
+  ];
   }
 
   getReports() {
-    const sub = this.reportsService.getReports().subscribe(
-      data => this.reportShorts = data,
+      console.log('getReports');
+      this.loader.start();
+      const sub = this.reportsService.getReports().subscribe(
+      data => {this.reportShorts = data; console.log('data loaded'); this.loader.stop(); },
       () => console.log('Error getReports'),
       () => sub.unsubscribe);
   }
 
-  deleteReport(id: string) {
-    const sub = this.reportsService.deleteReport(id).subscribe(
+  deleteReport() {
+    console.log(this.selectedReport.Id.toString());
+    return;
+    const sub = this.reportsService.deleteReport(this.selectedReport.Id.toString()).subscribe(
       () => null,
       () => console.log('Error deleteReport'),
       () => {
@@ -42,4 +66,19 @@ export class ReportsComponent implements OnInit {
       }
     );
   }
+
+  editReport() {
+    console.log( 'Edit ' + this.selectedReport.Id.toString());
+    return;
+  }
+
+  changeReport() {
+    if (this.selectedReport == null) { return; }
+    console.log(this.selectedReport);
+  }
+
+  clearFilter(dropdown: Dropdown, button: Button) {
+    dropdown.resetFilter();
+    button.disabled = true;
+}
 }
