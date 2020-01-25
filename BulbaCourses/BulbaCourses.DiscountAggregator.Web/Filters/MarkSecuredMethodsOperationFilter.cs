0ecc9d5dc9eb8535bb157be753eecd59950d23f7
Lapaev Igor <lapaev-tt@hotmail.com>
2020-01-25 23:45:17 +0300

    * swagger authentication

--- BulbaCourses/BulbaCourses.DiscountAggregator.Web/Filters/MarkSecuredMethodsOperationFilter.cs
+++ BulbaCourses/BulbaCourses.DiscountAggregator.Web/Filters/MarkSecuredMethodsOperationFilter.cs
@@ -11,23 +11,39 @@ namespace BulbaCourses.DiscountAggregator.Web
     {
         public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
         {
-            var filterPipeline = apiDescription.ActionDescriptor.GetFilterPipeline();
-            // check if authorization is required
-            var isAuthorized = filterPipeline
-                .Select(filterInfo => filterInfo.Instance)
-                .Any(filter => filter is IAuthorizationFilter);
-            // check if anonymous access is allowed
-            var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
-            if (isAuthorized && !allowAnonymous)
+            var filters = apiDescription.ActionDescriptor.GetFilterPipeline();
+
+            if (!filters.Select(f => f.Instance).OfType<AuthorizeAttribute>().Any())
             {
-                if (operation.security == null)
-                    operation.security = new List<IDictionary<string, IEnumerable<string>>>();
-                var auth = new Dictionary<string, IEnumerable<string>>
-        {
-            {"basic", Enumerable.Empty<string>()}
-        };
-                operation.security.Add(auth);
+                return;
             }
+
+            operation.security = operation.security ?? new List<IDictionary<string, IEnumerable<string>>>();
+            operation.security.Add(new Dictionary<string, IEnumerable<string>>()
+            {
+                { "oauth2", new List<string>(){ "openid", "profile", "api"}}
+            });
         }
+
+        //public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        //{
+        //    var filterPipeline = apiDescription.ActionDescriptor.GetFilterPipeline();
+        //    // check if authorization is required
+        //    var isAuthorized = filterPipeline
+        //        .Select(filterInfo => filterInfo.Instance)
+        //        .Any(filter => filter is IAuthorizationFilter);
+        //    // check if anonymous access is allowed
+        //    var allowAnonymous = apiDescription.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+        //    if (isAuthorized && !allowAnonymous)
+        //    {
+        //        if (operation.security == null)
+        //            operation.security = new List<IDictionary<string, IEnumerable<string>>>();
+        //        var auth = new Dictionary<string, IEnumerable<string>>
+        //{
+        //    {"basic", Enumerable.Empty<string>()}
+        //};
+        //        operation.security.Add(auth);
+        //    }
+        //}
     }
 }
