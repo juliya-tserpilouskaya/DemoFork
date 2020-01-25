3147307177ffd4687b048a1b4668f914b6d95cb3
Fedor Saevich <f.saevich@gmail.com>
2020-01-25 21:27:45 +0300

    + Add form for adding a learning course

--- bulbaCoursesClient/src/app/GlobalSearch/components/search/search.component.ts
+++ bulbaCoursesClient/src/app/GlobalSearch/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchService, Courses } from '../../services/search.service';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search',
@@ -9,8 +11,49 @@ import { SearchService, Courses } from '../../services/search.service';
 export class SearchComponent {
 
   query: string = "";
+  public isCollapsed = false;
+  public myForm: FormGroup;
 
-  constructor() {
-    // this.query = queryService.query;
+  constructor(private _fb: FormBuilder) {
+    this.myForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      items: this._fb.array([
+          this.initItem(),
+      ])
+  });
   }
+
+  initItem() {
+    return this._fb.group({
+        name: ['', Validators.required],
+        description: ['']
+    });
+  }
+
+  addItem() {
+    const control = <FormArray>this.myForm.controls['items'];
+    control.push(this.initItem());
+  }
+
+  removeItem(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.myForm.controls['items'];
+    control.removeAt(i);
+  }
+
+  save(model: Course) {
+    // call API to save customer
+    console.log(model);
+  }
+}
+
+export interface Course {
+  name: string;
+  description: string;
+  items: Items[];
+}
+
+export interface Items {
+  name: string;
+  description: string;
 }
