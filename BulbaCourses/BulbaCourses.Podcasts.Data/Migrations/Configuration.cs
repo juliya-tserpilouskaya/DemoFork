0736c19128e3daa99f0afef2791a357c02ba8502
Golgovskiy <dr.jir@mail.ru>
2020-01-26 22:43:56 +0300

    +default user

--- BulbaCourses/BulbaCourses.Podcasts.Data/Migrations/Configuration.cs
+++ BulbaCourses/BulbaCourses.Podcasts.Data/Migrations/Configuration.cs
@@ -22,24 +22,12 @@ namespace BulbaCourses.Podcasts.Data.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            var course1 = new CourseDb()
-            {
-                Id = "0",
-                Name = "TestName",
-                Raiting = 0,
-                Description = "test",
-                Duration = 13,
-                Price = 13,
-                Comments = null,
-                Audios = null,
-                CreationDate = DateTime.Now,
-                Author = null,
-            };
+            
             var user1 = new UserDb()
             {
-                Id = "0",
+                Id = "8C7362B6-AAD5-42F7-B366-CE45304D03A5",
                 IsAdmin = true,
-                Name = "TestUser",
+                Name = "user@test.com",
                 Avatar = null,
                 BoughtCourses = null,
                 UploadedCourses = null,
@@ -49,50 +37,7 @@ namespace BulbaCourses.Podcasts.Data.Migrations
                 Email = "",
             };
 
-            var audio1 = new AudioDb()
-            {
-                Id = "0",
-                Name = "Test",
-                Duration = 13,
-                Content = "0",
-                Course = null,
-            };
-            var comment1 = new CommentDb()
-            {
-                Id = "0",
-                Text = "TestComment",
-                Course = null,
-                PostDate = DateTime.Now,
-                User = null,
-            };
-            var content1 = new ContentDb()
-            {
-                Id = "0",
-                Data = "qwerty",
-                Audio = null,
-                
-            };
-
-            course1.Author = user1;
-            audio1.Course = course1;
-            comment1.User = user1;
-            comment1.Course = course1;
-            audio1.Content = content1.Id;
-
-            course1.Audios.Add(audio1);
-            course1.Comments.Add(comment1);
-            user1.UploadedCourses.Add(course1);
-
-            context.Courses.Add(course1);
-            context.Audios.Add(audio1);
-            context.Comments.Add(comment1);
             context.Users.Add(user1);
-            context.Courses.Add(course1);
-
-            var man = new ContentManager(new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter());
-            Task.FromResult(man.AddAsync(content1));
-            
-
             base.Seed(context);
         }
     }
