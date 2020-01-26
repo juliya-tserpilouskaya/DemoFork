65e171a051d16df441e73def65578312d0c0de48
KozyrickiS <skozyricki@gmail.com>
2020-01-26 21:00:37 +0300

    + add summary to repositories

--- BulbaCourses/BulbaCourses.Video.Data/Repositories/CourseRepository.cs
+++ BulbaCourses/BulbaCourses.Video.Data/Repositories/CourseRepository.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 
 namespace BulbaCourses.Video.Data.Repositories
 {
+    /// <summary>
+    /// Provides a mechanism for working course repository.
+    /// </summary>
     public class CourseRepository : BaseRepository, ICourseRepository
     {
 
@@ -18,6 +21,11 @@ namespace BulbaCourses.Video.Data.Repositories
         {
         }
 
+        /// <summary>
+        /// Create new course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public void Add(CourseDb course)
         {
             _videoDbContext.Courses.Add(course);
@@ -25,6 +33,11 @@ namespace BulbaCourses.Video.Data.Repositories
 
         }
 
+        /// <summary>
+        /// Create new course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public async Task<CourseDb> AddAsync(CourseDb course)
         {
             _videoDbContext.Courses.Add(course);
@@ -32,6 +45,10 @@ namespace BulbaCourses.Video.Data.Repositories
             return await Task.FromResult(course);
         }
 
+        /// <summary>
+        /// Gets all courses in repository.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<CourseDb> GetAll()
         {
             var courseList = _videoDbContext.Courses.ToList().AsReadOnly();
@@ -39,17 +56,32 @@ namespace BulbaCourses.Video.Data.Repositories
 
         }
 
+        /// <summary>
+        /// Gets all courses in repository.
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<CourseDb>> GetAllAsync()
         {
             var courseList = await _videoDbContext.Courses.ToListAsync().ConfigureAwait(false);
             return courseList.AsReadOnly();
         }
 
+        /// <summary>
+        /// Gets all videos from course by id in repository.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<VideoMaterialDb>> GetCoursesAsync(string id)
         {
             var videos = await _videoDbContext.Courses.Where(c => c.CourseId.Equals(id))?.SelectMany(c => c.Videos).ToListAsync();
             return videos.AsReadOnly(); ;
         }
+
+        /// <summary>
+        /// Gets all author courses by author in repository.
+        /// </summary>
+        /// <param name="author"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<CourseDb>> GetByAuthorAsync(AuthorDb author)
         {
             var courses = await _videoDbContext.Authors.Where(c => c.AuthorId == author.AuthorId)?.SelectMany(c => c.AuthorCourses).ToListAsync();
@@ -57,6 +89,11 @@ namespace BulbaCourses.Video.Data.Repositories
             return courses.AsReadOnly(); ;
         }
 
+        /// <summary>
+        /// Shows course details by id in repository.
+        /// </summary>
+        /// <param name="courseId"></param>
+        /// <returns></returns>
         public CourseDb GetById(string courseId)
         {
             var course = _videoDbContext.Courses.FirstOrDefault(b => b.CourseId.Equals(courseId));
@@ -64,12 +101,22 @@ namespace BulbaCourses.Video.Data.Repositories
 
         }
 
+        /// <summary>
+        /// Shows course details by id in repository.
+        /// </summary>
+        /// <param name="courseId"></param>
+        /// <returns></returns>
         public async Task<CourseDb> GetByIdAsync(string courseId)
         {
             var course = await _videoDbContext.Courses.SingleOrDefaultAsync(b => b.CourseId.Equals(courseId)).ConfigureAwait(false);
             return course;
         }
 
+        /// <summary>
+        /// Remove course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public void Remove(CourseDb course)
         {
             _videoDbContext.Courses.Remove(course);
@@ -77,6 +124,11 @@ namespace BulbaCourses.Video.Data.Repositories
 
         }
 
+        /// <summary>
+        /// Remove course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public async Task RemoveAsync(CourseDb course)
         {
             if (course == null)
@@ -87,6 +139,11 @@ namespace BulbaCourses.Video.Data.Repositories
             await _videoDbContext.SaveChangesAsync().ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Remove course by id in repository.
+        /// </summary>
+        /// <param name="courseId"></param>
+        /// <returns></returns>
         public async Task RemoveAsyncById(string courseId)
         {
             var course = _videoDbContext.Courses.SingleOrDefault(c => c.CourseId.Equals(courseId));
@@ -98,6 +155,11 @@ namespace BulbaCourses.Video.Data.Repositories
             await _videoDbContext.SaveChangesAsync().ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Update course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public void Update(CourseDb course)
         {
             if (course == null)
@@ -109,6 +171,11 @@ namespace BulbaCourses.Video.Data.Repositories
 
         }
 
+        /// <summary>
+        /// Update course in repository.
+        /// </summary>
+        /// <param name="course"></param>
+        /// <returns></returns>
         public async Task<CourseDb> UpdateAsync(CourseDb course)
         {
             if (course == null)
@@ -120,6 +187,11 @@ namespace BulbaCourses.Video.Data.Repositories
             return await Task.FromResult(course);
         }
 
+        /// <summary>
+        /// Check if course exists with this Name.
+        /// </summary>
+        /// <param name="courseName"></param>
+        /// <returns></returns>
         public async Task<bool> IsNameExistAsync(string courseName)
         {
             return await _videoDbContext.Courses.AnyAsync(c => c.Name.Equals(courseName)).ConfigureAwait(false);
