48b15306c0734de6c32b1eaf11b8ccd6516a01aa
KozyrickiS <skozyricki@gmail.com>
2020-01-26 20:41:30 +0300

    * change swagger authentification

--- BulbaCourses/BulbaCourses.Video.Web/App_Start/SwaggerConfig.cs
+++ BulbaCourses/BulbaCourses.Video.Web/App_Start/SwaggerConfig.cs
@@ -5,6 +5,7 @@ using Swashbuckle.Application;
 using Swashbuckle.Examples;
 using System;
 using System.Xml.XPath;
+using BulbaCourses.Video.Web.Filters;
 
 namespace BulbaCourses.Video.Web
 {
@@ -34,74 +35,74 @@ namespace BulbaCourses.Video.Web
                 //
                 c.SingleApiVersion("v1", "BulbaCourses.Video.Web");
 
-                        // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
-                        //
-                        //c.PrettyPrint();
+                // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
+                //
+                //c.PrettyPrint();
 
-                        // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
-                        // In this case, you must provide a lambda that tells Swashbuckle which actions should be
-                        // included in the docs for a given API version. Like "SingleApiVersion", each call to "Version"
-                        // returns an "Info" builder so you can provide additional metadata per API version.
-                        //
-                        //c.MultipleApiVersions(
-                        //    (apiDesc, targetApiVersion) => ResolveVersionSupportByRouteConstraint(apiDesc, targetApiVersion),
-                        //    (vc) =>
-                        //    {
-                        //        vc.Version("v2", "Swashbuckle Dummy API V2");
-                        //        vc.Version("v1", "Swashbuckle Dummy API V1");
-                        //    });
+                // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
+                // In this case, you must provide a lambda that tells Swashbuckle which actions should be
+                // included in the docs for a given API version. Like "SingleApiVersion", each call to "Version"
+                // returns an "Info" builder so you can provide additional metadata per API version.
+                //
+                //c.MultipleApiVersions(
+                //    (apiDesc, targetApiVersion) => ResolveVersionSupportByRouteConstraint(apiDesc, targetApiVersion),
+                //    (vc) =>
+                //    {
+                //        vc.Version("v2", "Swashbuckle Dummy API V2");
+                //        vc.Version("v1", "Swashbuckle Dummy API V1");
+                //    });
 
-                        // You can use "BasicAuth", "ApiKey" or "OAuth2" options to describe security schemes for the API.
-                        // See https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md for more details.
-                        // NOTE: These only define the schemes and need to be coupled with a corresponding "security" property
-                        // at the document or operation level to indicate which schemes are required for an operation. To do this,
-                        // you'll need to implement a custom IDocumentFilter and/or IOperationFilter to set these properties
-                        // according to your specific authorization implementation
-                        //
-                        //c.BasicAuth("basic")
-                        //    .Description("Basic HTTP Authentication");
-                        //
-                        // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
-                        //c.ApiKey("apiKey")
-                        //    .Description("API Key Authentication")
-                        //    .Name("apiKey")
-                        //    .In("header");
-                        //
-                        //c.OAuth2("oauth2")
-                        //    .Description("OAuth2 Implicit Grant")
-                        //    .Flow("implicit")
-                        //    .AuthorizationUrl("http://petstore.swagger.wordnik.com/api/oauth/dialog")
-                        //    //.TokenUrl("https://tempuri.org/token")
-                        //    .Scopes(scopes =>
-                        //    {
-                        //        scopes.Add("read", "Read access to protected resources");
-                        //        scopes.Add("write", "Write access to protected resources");
-                        //    });
+                // You can use "BasicAuth", "ApiKey" or "OAuth2" options to describe security schemes for the API.
+                // See https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md for more details.
+                // NOTE: These only define the schemes and need to be coupled with a corresponding "security" property
+                // at the document or operation level to indicate which schemes are required for an operation. To do this,
+                // you'll need to implement a custom IDocumentFilter and/or IOperationFilter to set these properties
+                // according to your specific authorization implementation
+                //
+                c.BasicAuth("basic")
+                    .Description("Basic HTTP Authentication");
+                //
+                // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
+                //c.ApiKey("apiKey")
+                //    .Description("API Key Authentication")
+                //    .Name("apiKey")
+                //    .In("header");
+                //
+                c.OAuth2("oauth2")
+                    .Description("OAuth2 Implicit Grant")
+                    .Flow("implicit")
+                    .AuthorizationUrl("http://localhost:44382/connect/authorize")
+                    .TokenUrl("http://localhost:44382/connect/token")
+                    .Scopes(scopes =>
+                    {
+                        scopes.Add("openid", "Read access to protected resources");
+                        scopes.Add("profile", "Write access to protected resources");
+                    });
 
-                        // Set this flag to omit descriptions for any actions decorated with the Obsolete attribute
-                        //c.IgnoreObsoleteActions();
+                // Set this flag to omit descriptions for any actions decorated with the Obsolete attribute
+                //c.IgnoreObsoleteActions();
 
-                        // Each operation be assigned one or more tags which are then used by consumers for various reasons.
-                        // For example, the swagger-ui groups operations according to the first tag of each operation.
-                        // By default, this will be controller name but you can use the "GroupActionsBy" option to
-                        // override with any value.
-                        //
-                        //c.GroupActionsBy(apiDesc => apiDesc.HttpMethod.ToString());
+                // Each operation be assigned one or more tags which are then used by consumers for various reasons.
+                // For example, the swagger-ui groups operations according to the first tag of each operation.
+                // By default, this will be controller name but you can use the "GroupActionsBy" option to
+                // override with any value.
+                //
+                //c.GroupActionsBy(apiDesc => apiDesc.HttpMethod.ToString());
 
-                        // You can also specify a custom sort order for groups (as defined by "GroupActionsBy") to dictate
-                        // the order in which operations are listed. For example, if the default grouping is in place
-                        // (controller name) and you specify a descending alphabetic sort order, then actions from a
-                        // ProductsController will be listed before those from a CustomersController. This is typically
-                        // used to customize the order of groupings in the swagger-ui.
-                        //
-                        //c.OrderActionGroupsBy(new DescendingAlphabeticComparer());
+                // You can also specify a custom sort order for groups (as defined by "GroupActionsBy") to dictate
+                // the order in which operations are listed. For example, if the default grouping is in place
+                // (controller name) and you specify a descending alphabetic sort order, then actions from a
+                // ProductsController will be listed before those from a CustomersController. This is typically
+                // used to customize the order of groupings in the swagger-ui.
+                //
+                //c.OrderActionGroupsBy(new DescendingAlphabeticComparer());
 
-                        // If you annotate Controllers and API Types with
-                        // Xml comments (http://msdn.microsoft.com/en-us/library/b2s063f7(v=vs.110).aspx), you can incorporate
-                        // those comments into the generated docs and UI. You can enable this by providing the path to one or
-                        // more Xml comment files.
-                        //
-                        c.IncludeXmlComments(GetXmlCommentsPath());
+                // If you annotate Controllers and API Types with
+                // Xml comments (http://msdn.microsoft.com/en-us/library/b2s063f7(v=vs.110).aspx), you can incorporate
+                // those comments into the generated docs and UI. You can enable this by providing the path to one or
+                // more Xml comment files.
+                //
+                c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -156,9 +157,9 @@ namespace BulbaCourses.Video.Web
                         // to inspect some attribute on each action and infer which (if any) OAuth2 scopes are required
                         // to execute the operation
                         //
-                        //c.OperationFilter<AssignOAuth2SecurityRequirements>();
+                        c.OperationFilter<MarkSecuredMethodsOperationFilter>();
                         c.OperationFilter<ExamplesOperationFilter>();
-                        // Post-modify the entire Swagger document by wiring up one or more Document filters.
+                        // Post-modify the entire Swagger document by wiring up one or more Document filters.IncludeXmlComments
                         // This gives full control to modify the final SwaggerDocument. You should have a good understanding of
                         // the Swagger 2.0 spec. - https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
                         // before using this option.
@@ -232,18 +233,18 @@ namespace BulbaCourses.Video.Web
                         // a discovery URL for each version. This provides a convenient way for users to browse documentation
                         // for different API versions.
                         //
-                        //c.EnableDiscoveryUrlSelector();
+                        c.EnableDiscoveryUrlSelector();
 
                         // If your API supports the OAuth2 Implicit flow, and you've described it correctly, according to
                         // the Swagger 2.0 specification, you can enable UI support as shown below.
                         //
-                        //c.EnableOAuth2Support(
-                        //    clientId: "test-client-id",
-                        //    clientSecret: null,
-                        //    realm: "test-realm",
-                        //    appName: "Swagger UI"
-                        //    //additionalQueryStringParams: new Dictionary<string, string>() { { "foo", "bar" } }
-                        //);
+                        c.EnableOAuth2Support(
+                            clientId: "external_app",
+                            clientSecret: null,
+                            realm: "test-realm",
+                            appName: "Swagger UI"
+                        //additionalQueryStringParams: new Dictionary<string, string>() { { "foo", "bar" } }
+                        );
 
                         // If your API supports ApiKey, you can override the default values.
                         // "apiKeyIn" can either be "query" or "header"
